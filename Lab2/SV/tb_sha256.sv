39b4e5c4933a8d11a9f17be621d971a3f3109b44
lamckay1 <157834324+lamckay1@users.noreply.github.com>
2024-09-27 13:29:57 -0500

    Update tb_sha256.sv

--- Lab2/SV/tb_sha256.sv
+++ Lab2/SV/tb_sha256.sv
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 module stimulus;
 
-   parameter MSG_SIZE = 120;   
+   parameter MSG_SIZE = 24;   
 
    logic [MSG_SIZE-1:0] message;   
    logic [255:0] hashed;
@@ -40,9 +40,9 @@ module stimulus;
    always @(posedge clk)
      begin
 	// Add message here : "Hello, SHA-256!"	
-	      #1 message = 120'h48656c6c6f2c205348412d32353621;
+	      #1 message = 24'h616263;
 	// Expected result 
-        #0 result = 256'hd0e8b8f11c98f369016eb2ed3c541e1f01382f9d5b3104c9ffd06b6175a46271;	
+        #0 result = 256'hba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad;	
      end  
 
    // check results on falling edge of clk
