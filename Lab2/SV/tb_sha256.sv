4c64002c85d0f0208506c209ae39a85b651ffd08
lamckay1 <157834324+lamckay1@users.noreply.github.com>
2024-10-10 10:20:24 -0500

    Update tb_sha256.sv

--- Lab2/SV/tb_sha256.sv
+++ Lab2/SV/tb_sha256.sv
@@ -3,24 +3,38 @@ module stimulus;
 
    parameter MSG_SIZE = 24;   
 
-   logic [MSG_SIZE-1:0] message;   
-   logic [255:0] hashed;
+   logic [MSG_SIZE-1:0] message1;   
+   logic [255:0] hashed1;
+   logic [MSG_SIZE-1:0] message2;   
+   logic [255:0] hashed2;  
+   logic [MSG_SIZE-1:0] message3;   
+   logic [255:0] hashed3;  
+   logic [MSG_SIZE-1:0] message4;   
+   logic [255:0] hashed4;
+   logic [MSG_SIZE-1:0] message5;   
+   logic [255:0] hashed5;
 
    logic 	 clk;
    logic [31:0]  errors;
    logic [31:0]  vectornum;
-   logic [255:0]  result;
+   logic [255:0]  result1;
+   logic [255:0]  result2;
+   logic [255:0]  result3;
+   logic [255:0]  result4;
+   logic [255:0]  result5;
    // Size of [351:0] is size of vector in file: 96 + 256 = 352 bits
    logic [351:0] testvectors[511:0];
-   
    integer 	 handle3;
    integer 	 desc3;
    integer 	 i;  
    integer       j;
 
    // Instantiate DUT
-   top #(MSG_SIZE, 512) dut (message, hashed);
-
+   top #(MSG_SIZE, 512) dut1 (message1, hashed1);
+   top #(MSG_SIZE, 512) dut2 (message2, hashed2);
+   top #(MSG_SIZE, 512) dut3 (message3, hashed3);
+   top #(MSG_SIZE, 512) dut4 (message4, hashed4);
+   top #(MSG_SIZE, 512) dut5 (message5, hashed5);
    // 1 ns clock
    initial 
      begin	
@@ -34,24 +48,31 @@ module stimulus;
 	vectornum = 0;
 	errors = 0;		
 	desc3 = handle3;
+     $readmemh("sha256.tv", testvectors, 0, 4);
      end
+
+     
 
     // apply test vectors on rising edge of clk
    always @(posedge clk)
      begin
-	// Add message here : "Hello, SHA-256!"	
-	      #1 message = 24'h616263;
+	// Add message here : "abc"	
+	      #1 message1 = testvectors[vectornum];
 	// Expected result 
-        #0 result = 256'h78a30be97918f2895825904585c04db79881ded24f8629c0c728e805e8577aa7;	
+        #0 result1 = 256'hba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad;	
+	
      end  
+
+     
+      
 
    // check results on falling edge of clk
    always @(negedge clk)
      begin
-	if (result != hashed)
+	if (result1 != hashed1)
           errors = errors + 1;
-        $fdisplay(desc3, "message: %h\nhashed: %h\nresult: %h\nmatching: %b", 
-                  message, hashed, result, (result == hashed));
+        $fdisplay(desc3, "message: %h\nhashed: %h\nresult: %h\nmatching: %b\n\n", 
+                  message1, hashed1, result1, (result1 == hashed1));
 	vectornum = vectornum + 1;
 	if (testvectors[vectornum] === 352'bx) 
           begin 
@@ -59,6 +80,7 @@ module stimulus;
                       vectornum, errors);
              $finish;
           end
-     end   
+     end   //1
+
 
 endmodule // stimulus
