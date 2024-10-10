6c1b6139f0f205d0c1f34295ffe50201da82b2a4
lamckay1 <157834324+lamckay1@users.noreply.github.com>
2024-10-10 10:30:46 -0500

    Update tb_sha256.sv

--- Lab2/SV/tb_sha256.sv
+++ Lab2/SV/tb_sha256.sv
@@ -5,25 +5,14 @@ module stimulus;
 
    logic [MSG_SIZE-1:0] message1;   
    logic [255:0] hashed1;
-   logic [MSG_SIZE-1:0] message2;   
-   logic [255:0] hashed2;  
-   logic [MSG_SIZE-1:0] message3;   
-   logic [255:0] hashed3;  
-   logic [MSG_SIZE-1:0] message4;   
-   logic [255:0] hashed4;
-   logic [MSG_SIZE-1:0] message5;   
-   logic [255:0] hashed5;
+   
 
    logic 	 clk;
    logic [31:0]  errors;
    logic [31:0]  vectornum;
    logic [255:0]  result1;
-   logic [255:0]  result2;
-   logic [255:0]  result3;
-   logic [255:0]  result4;
-   logic [255:0]  result5;
    // Size of [351:0] is size of vector in file: 96 + 256 = 352 bits
-   logic [351:0] testvectors[511:0];
+   logic [279:0] testvectors[511:0];
    integer 	 handle3;
    integer 	 desc3;
    integer 	 i;  
@@ -31,10 +20,6 @@ module stimulus;
 
    // Instantiate DUT
    top #(MSG_SIZE, 512) dut1 (message1, hashed1);
-   top #(MSG_SIZE, 512) dut2 (message2, hashed2);
-   top #(MSG_SIZE, 512) dut3 (message3, hashed3);
-   top #(MSG_SIZE, 512) dut4 (message4, hashed4);
-   top #(MSG_SIZE, 512) dut5 (message5, hashed5);
    // 1 ns clock
    initial 
      begin	
@@ -57,9 +42,9 @@ module stimulus;
    always @(posedge clk)
      begin
 	// Add message here : "abc"	
-	      #1 message1 = testvectors[vectornum];
+	      #1 message1 = testvectors[vectornum][279:256];
 	// Expected result 
-        #0 result1 = 256'hba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad;	
+        #0 result1 = testvectors[vectornum][255:0];	
 	
      end  
 
@@ -74,7 +59,7 @@ module stimulus;
         $fdisplay(desc3, "message: %h\nhashed: %h\nresult: %h\nmatching: %b\n\n", 
                   message1, hashed1, result1, (result1 == hashed1));
 	vectornum = vectornum + 1;
-	if (testvectors[vectornum] === 352'bx) 
+	if (testvectors[vectornum] === {280{1'bx}}) 
           begin 
              $display("%d tests completed with %d errors", 
                       vectornum, errors);
