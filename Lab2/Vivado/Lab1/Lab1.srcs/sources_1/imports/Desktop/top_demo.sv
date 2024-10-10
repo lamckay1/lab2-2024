ad5830b0557647a3a064cf1884866030ece61834
lamckay1 <157834324+lamckay1@users.noreply.github.com>
2024-10-10 11:40:36 -0500

    Update top_demo.sv

--- Lab2/Vivado/Lab1/Lab1.srcs/sources_1/imports/Desktop/top_demo.sv
+++ Lab2/Vivado/Lab1/Lab1.srcs/sources_1/imports/Desktop/top_demo.sv
@@ -55,10 +55,23 @@ module top_demo
   
   alway_comb begin
         case(sw[1:0])
-        4'b00: s = result[63:0];
-        4'b01: s = result[127:64];
-        4'b10: s = result[191:128];
-        4'b11: s = result[255:192];
+          4'b0000: s = result[15:0];
+          4'b0001: s = result[31:16];
+          4'b0010: s = result[47:32];
+          4'b0011: s = result[63:48];
+          4'b0100: s = result[79:64];
+          4'b0101: s = result[95:80];
+          4'b0110: s = result[111:96];
+          4'b0111: s = result[127:112];
+          4'b1000: s = result[143:128];
+          4'b1001: s = result[159:144];
+          4'b1010: s = result[175:160];
+          4'b1011: s = result[191:176];
+          4'b1100: s = result[207:192];
+          4'b1101: s = result[223:208];
+          4'b1110: s = result[239:224];
+          4'b1111: s = result[255:240];
+          
         
         endcase
   end
